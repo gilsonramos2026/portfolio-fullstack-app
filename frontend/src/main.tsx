f5e7181343841e2cba9c6c5b086e47581e964301
Gilson Ramos <gilsrms0@gmail.com>
2026-06-09 15:16:07 -0300

    feat: bind BrowserRouter inside App component to match root providers config

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { AppRoutes } from './routes' // Importa a árvore de rotas separada
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+export default function App() {
+  return (
+    <BrowserRouter>
+      {/* O Roteador fica aqui para alimentar as rotas que isolamos no outro arquivo */}
+      <AppRoutes />
+    </BrowserRouter>
+  )
+}
