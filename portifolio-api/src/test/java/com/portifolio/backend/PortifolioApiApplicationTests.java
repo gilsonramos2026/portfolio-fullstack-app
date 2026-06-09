ae2fd81772cc50522624730bedcb80e9a63c3a6e
Gilson Ramos <gilsrms0@gmail.com>
2026-06-09 10:51:52 -0300

    feat: adicionar configuracao da api e entidade de projetos

---
+++ portifolio-api/src/test/java/com/portifolio/backend/PortifolioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portifolio.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortifolioApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
