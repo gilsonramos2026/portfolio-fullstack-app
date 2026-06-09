8670e91f3afd39656236877ff444bf2d6c0a7cc2
Gilson Ramos <gilsrms0@gmail.com>
2026-06-09 16:51:21 -0300

    feat: complete full-stack portfolio integration with updated react 19, vite 8 and java spring boot layers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
-
-import { AppRoutes } from './routes'
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { AppRoutes } from './routes' // Puxa a árvore isolada de caminhos
 
 export default function App() {
   return (
-    <>
+    <BrowserRouter>
+      {/* O Roteador DEVE morar aqui para fornecer contexto para o AppRoutes */}
       <AppRoutes />
-    </>
+    </BrowserRouter>
   )
 }
