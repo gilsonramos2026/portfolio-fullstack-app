cadc8a7baa5533927475f012b1f37d4fd2ec0c98
Gilson Ramos <gilsrms0@gmail.com>
2026-09-09 13:30:34 -0300

    refactor: update Address entity properties

--- backend/src/main/java/com/portfolio/domain/Address.java
+++ backend/src/main/java/com/portfolio/domain/Address.java
@@ -1,5 +1,6 @@
 package com.portfolio.domain;
 
+import com.portfolio.domain.enums.AddressType;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
